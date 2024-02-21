1252b35107f7d0cf19d59bff10c3871a10228c0a
saranya <saranyasivakumarsvks@gmail.com>
2024-02-21 14:00:47 +0530

    Countryflag , name , subregion , region .

---
+++ scri.js
@@ -0,0 +1,10 @@
+
+
+ const obj1 = {name: "person1", age: 5}
+ const obj2 = {age: 5, name: "person1"}
+
+ console.log(obj1 === obj2);
+const keys1 = object.keys(obj1);
+const keys2 = object.keys(obj2);
+
+console.log(keys1 === keys2);
